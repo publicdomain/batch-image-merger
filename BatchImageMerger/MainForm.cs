8ebf623d2940bafc00a84e7dc0bdb54c372c3cf8
Victor <publicdomainvic@gmail.com>
2022-06-16 11:48:18 -0400

    Add OnExitToolStripMenuItemClick()

--- BatchImageMerger/MainForm.cs
+++ BatchImageMerger/MainForm.cs
@@ -476,7 +476,8 @@ namespace BatchImageMerger
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Close program
+            this.Close();
         }
     }
 }
