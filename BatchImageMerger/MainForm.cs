2225f2ea57b611098e2b9ee51e8b82e65c2a7401
Victor <publicdomainvic@gmail.com>
2022-06-17 12:08:46 -0400

    Add OnSourceCodeGithubcomToolStripMenuItemClick()

--- BatchImageMerger/MainForm.cs
+++ BatchImageMerger/MainForm.cs
@@ -285,7 +285,8 @@ namespace BatchImageMerger
         /// <param name="e">Event arguments.</param>
         private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open GitHub repository
+            Process.Start("https://github.com/publicdomain/batch-image-merger");
         }
 
         /// <summary>
